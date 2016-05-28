790e9338c447bebd615f3ae7ea63cdb3f330ce8a
apple <apple@songdaozhang.local>
2016-05-28 17:18:43 +0800

    nidayessss

---
+++ nidaye3/nidaye3/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  nidaye3
+//
+//  Created by apple on 16/5/28.
+//  Copyright © 2016年 戴维营. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
